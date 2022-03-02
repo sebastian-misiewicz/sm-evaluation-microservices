f6085c3c5e921c8ced6a2406bea784024b93a0ee
Sebastian Misiewicz <sebastian.misiewicz.external@atos.net>
2022-03-02 14:10:00 +0100

    Add a basic ms-server

---
+++ sm-evaluation-ms-server/src/main/java/sm/evaluation/server/SmEvaluationMsServerApplication.java
@@ -0,0 +1,14 @@
+package sm.evaluation.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmEvaluationMsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmEvaluationMsServerApplication.class, args);
+		System.out.println("Test");
+	}
+
+}
